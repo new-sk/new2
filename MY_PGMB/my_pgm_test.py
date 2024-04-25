79c81ab3e29213c39f5d5e2db11769a00a75afd1
new-sk <sookyun9@daum.net>
2024-04-25 21:43:30 +0900

    MPB 240425L

--- MY_PGMB/my_pgm_test.py
+++ MY_PGMB/my_pgm_test.py
@@ -1,12 +1,5 @@
 import random
 
-sum = 0
-for i in range(1000):
-  sum += (random.randint(-100,100))
-print(sum)
-
-menus = ["짜장", "짦뽕", "볶음밥"]
-
-print(random.choice(menus))
-
-random.ch
+numlist = {'a':20, 'b':30}
+print(type(numlist))
+print(numlist)
