10a4d2f96e38fc1f66562e7563d5f59bfa4e6923
new-sk <email>
2024-06-16 18:47:25 +0900

    MPB2 240616H

--- MY_PGMB/py/my_schedule.py
+++ MY_PGMB/py/my_schedule.py
@@ -4,14 +4,24 @@
 # 3. 소스 별도 보관 : 소스, 인풋, 아웃풋
 # 4. 실적 파일 별도 관리 (아웃품과 또 다른)
 
+__istest__ = True
+
 import os
+import sys
 my_dir, my_file = os.path.split(__file__)
+
+os = os.name
+
+if os != "nt" and (not __istest__):
+  print("Not Allowed : Real Mode")
+  sys.exit()
 
 from datetime import datetime
 from dateutil.relativedelta import relativedelta  # 윤년 고려
 
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
 
 class DataFrameApp:
     def __init__(self, root, df):
@@ -98,19 +108,6 @@ print("today : " , today)
 dfy = df[(df['Cycle'] == 'Y')]
 dfy = dfy.copy()
 
-# 최근 1년 이내의 값이 아닌 것이 있는지 확인 : 조사안함 : NY일 경우 처리 방법이 모호함
-'''
-year1_ago = today - relativedelta(years=1)
-year1_next = today + relativedelta(years=1)
-year1_range = (dfy['Date'] < year1_ago) | (dfy['Date'] > year1_next)
-
-if year1_range.any():
-  print("="*50)
-  print("WARNING : 최근 1년 이내의 값이 아닌 데이터가 있습니다.")
-  print(dfy[year1_range])
-  print("="*50)
-'''
-
 # 연간 플랜 수립
 def update_year(row):
   if row['Date'] >= today:
@@ -170,24 +167,15 @@ dfw2 = dfw.copy()
 dfw2['Date'] = dfw2.apply(update_week, axis=1) # 각 행별로 날짜 변경
 dfw2['CycleMemo'] = '2nd ' + dfw2['Num'].astype(str) + ' Week'
 
-# 세번째 주간
-dfw3 = dfw2.copy()
-dfw3['Date'] = dfw3.apply(update_week, axis=1) # 각 행별로 날짜 변경
-dfw3['CycleMemo'] = 'Next ' + dfw3['Num'].astype(str) + ' Week(s)'
+# # 세번째 주간
+# dfw3 = dfw2.copy()
+# dfw3['Date'] = dfw3.apply(update_week, axis=1) # 각 행별로 날짜 변경
+# dfw3['CycleMemo'] = 'Next ' + dfw3['Num'].astype(str) + ' Week(s)'
 
-## 네번째 주간
-#dfw4 = dfw3.copy()
-#dfw4['Date'] = dfw4.apply(update_week, axis=1) # 각 행별로 날짜 변경
-#dfw4['CycleMemo'] = 'Next ' + dfw4['Num'].astype(str) + ' Week(s)'
-#
-## 다섯번째 주간
-#dfw5 = dfw4.copy()
-#dfw5['Date'] = dfw5.apply(update_week, axis=1) # 각 행별로 날짜 변경
-#dfw5['CycleMemo'] = 'Next ' + dfw5['Num'].astype(str) + ' Week(s)'
 
 print("Weekly Plan")
 #dfw = pd.concat([dfw,dfw2,dfw3,dfw4,dfw5])
-dfw = pd.concat([dfw,dfw2,dfw3])
+dfw = pd.concat([dfw,dfw2])
 print(dfw)
 
 
@@ -198,33 +186,48 @@ df = df.reset_index(drop=True)   # re-index
 print("Total Plan")
 print(df)
 
-# (WARNING) 오늘 이전날짜 계획이 있는가? 
-file_mode = 'w'
-if not df[ df['Date'] < today ].empty:
-  with open(my_dir + '/my_schedule_output.txt', 'w', encoding='utf-8') as f:
-    print("-"*50, file=f)
-    print("WARNING", file=f)
-    print( df[ df['Date'] < today ], file=f)
-  file_mode = 'a'
+# 현재 모드 출력 : Test & Not Test
+if __istest__ :
+  current_mode_print = "Test Mode"
+  fn_output = ""
+else :
+  current_mode_print = "Real Mode"
 
-# Generate OUTPUT file
-with open(my_dir + '/my_schedule_output.txt', file_mode, encoding='utf-8') as f:
+
+with open(my_dir + '/my_schedule_output.txt', 'w', encoding='utf-8') as f:
   print("-"*50, file=f)
+  print(current_mode_print, file=f)
+  print("-"*50, file=f)
+  
+
+# Only Test Mode : (WARNING) 오늘 이전날짜 계획이 있는가? 
+if __istest__ :
+  if not df[ df['Date'] < today ].empty:
+    with open(my_dir + '/my_schedule_output.txt', 'a', encoding='utf-8') as f:
+      print("WARNING", file=f)
+      print( df[ df['Date'] < today ], file=f)
+      print("-"*50, file=f)
+
+# This Month
+with open(my_dir + '/my_schedule_output.txt', 'a', encoding='utf-8') as f:
   print ("이번달(" + today.strftime('%Y-%m') + ") 일정", file=f)
   print( df[ (df['Date'].dt.year == today.year) & (df['Date'].dt.month == today.month) ], file=f)
-
-with open(my_dir + '/my_schedule_output.txt', 'a', encoding='utf-8') as f:
   print("-"*50, file=f)
+
+# Next Month
+with open(my_dir + '/my_schedule_output.txt', 'a', encoding='utf-8') as f:
   nextm_day = today + relativedelta(months=1)
   print ("다음달(" + nextm_day.strftime('%Y-%m') + ") 일정", file=f)
   print( df[ (df['Date'].dt.year == nextm_day.year) & (df['Date'].dt.month == nextm_day.month) ], file=f)
+  print("-"*50, file=f)
 
-# 이건 잠시만 막는거야...
-with open(my_dir + '/my_schedule_output.txt', 'a', encoding='utf-8') as f:
-  print("-"*50, file=f)
-  print("이후 일정", file=f)
-  print( df[ (df['Date'].dt.year > nextm_day.year)  |  (df['Date'].dt.year == nextm_day.year) & (df['Date'].dt.month > nextm_day.month) ].sort_values(by=["Cycle","Date"], ascending=[False,True]), file=f)
-  print("-"*50, file=f)
+# Only Test Mode : 이건 잠시만 막는거야...
+if __istest__ :
+  with open(my_dir + '/my_schedule_output.txt', 'a', encoding='utf-8') as f:
+    print("-"*50, file=f)
+    print("이후 일정", file=f)
+    print( df[ (df['Date'].dt.year > nextm_day.year)  |  (df['Date'].dt.year == nextm_day.year) & (df['Date'].dt.month > nextm_day.month) ].sort_values(by=["Cycle","Date"], ascending=[False,True]), file=f)
+    print("-"*50, file=f)
 
 
 # 데이터프레임 생성
