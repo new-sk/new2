fda8a0c656973a329e317871f5293b4c82cb32e4
new-sk <email>
2024-07-28 01:38:28 +0900

    MY9 240727H

--- MY_PGMB/py/my9_stock.py
+++ MY_PGMB/py/my9_stock.py
@@ -13,20 +13,13 @@ import pandas as pd
 
 import os
 import sys
-my_dir, my_file = os.path.split(__file__)
-my_sfile = '/my9_stock_input.txt'
 
-my_ldir = 'D:\MY_BLOG_LOCAL_HOME\py'
-os = os.name
 
-# False
-if (os == "nt") :
-  my_dir = my_ldir
-  max_loop = 0  # 전체 가져오기
-else :
-  # my_dir = 소스가 있는 여기
-  max_loop = 3  # 2페이지만 가져오기 (1,2)
-
+# 클래스 변수 초기화
+sMode = "Test"
+my_dir = ""
+my_sfile = ""
+max_loop = 3
 
 
 def get_all(sCode, oType):
@@ -177,16 +170,156 @@ def get_all(sCode, oType):
     df.to_csv(my_dir + my9_fname, index=False)
 
 
-"""  #1 : 특정 종목 전체 기간 데이터 가져오기
-### MAIN
-df = pd.read_csv(my_dir + '/my9_stock_inall.txt', dtype={'sCode': str})
+def get_one(sCode):
+  # 초기화 : 비어있는 배열
+  result_date = []
+  result_index = []
+
+  headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.272 Whale/2.9.118.16 Safari/537.36',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+      'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+      'Accept-Encoding': 'none',
+      'Accept-Language': 'en-US,en;q=0.8',
+      'Connection': 'keep-alive'}
+
+  # 읽어야 할 페이지
+  #   : KOSPI
+  if sCode == "KOSPI":
+    basic_url = "https://finance.naver.com/sise/sise_index_day.naver?code=KOSPI&page=1"
+  #   : 개별종목
+  else:
+    basic_url = "https://finance.naver.com/item/sise_day.naver?code=" + sCode + "&page=1" 
+  #print(basic_url)               
+              
+  # 웹 페이지 열기 / SOUP까지 수행
+  context = ssl._create_unverified_context()
+  req = urllib.request.Request(basic_url, headers = headers);  # 개별종목 처리 위해서는 이것이 필요함 : 아마도 막은 듯
+  with urllib.request.urlopen(req, context=context) as response:
+    # 바이너리 데이터 읽기
+    data = response.read()
+    # euc-kr 인코딩으로 디코딩  (NAVER에서 사용한 것임)
+    source = data.decode('euc-kr')
+    # source = data.decode('utf-8')
+
+  soup = BeautifulSoup(source, 'html.parser')
+
+
+  # 공통내역 : KOSPI
+  if sCode == "KOSPI":
+    # soup를 2번하다. 날짜 / 숫자값
+    # 1. 날짜
+    soup1 = soup.findAll("td",class_="date")
+    text_date = [tag.get_text(strip=True) for tag in soup1]
+    # 2. 숫자
+    soup2 = soup.findAll("td",class_="number_1")
+    text_index = [tag.get_text(strip=True) for tag in soup2]
+
+    # 겁나 짜증난다. 숫자는 종류가 많다. 매 4번째만 진짜다
+    # 추출할 인덱스 설정: 첫 번째 인덱스(0번 인덱스)와 매 4번째 인덱스
+    index_extract = list(range(0, len(text_index), 4))
+    # 새로운 배열에 추출된 값 저장
+    text_index = [text_index[i] for i in index_extract]
+
+  # 공통내역 : 개별종목
+  else:
+    # soup를 2번하다. 날짜 / 숫자값
+    # 1. TR 전체를 읽어들임
+    text_date  = []
+    text_index = []
+    soup1 = soup.find_all('tr', attrs={'onmouseout': True, 'onmouseover': True})
+    # print('')
+    # print(soup1)
+    # print('')
+
+    # 2. 날자와 지수 받아 오다
+    for row in soup1:
+      # 타겟 <span> 요소 다음의 형제 요소들 찾기
+      row_date = row.find('span', class_='tah p10 gray03')
+      row_index = row.find('span', class_='tah p11')  # 여러개 나오지만, 첫번째 것만 활용
+
+      if row_date is not None:
+        text_date.append(row_date.text)
+        text_index.append(row_index.text)
+        #print(row_date.text, row_index.text)
+      
+
+  result_date = text_date
+  result_index = text_index
+
+    
+  # 데이터프레임 생성
+  dfone = pd.DataFrame({
+      'sCode': sCode,
+      'Date': result_date,
+      'Price': result_index
+  })
+
+  my9_fname = '/my9_stock_out_' + sCode + '.txt'
+  dfall = pd.read_csv(my_dir + my9_fname, dtype={'sCode': str})
+
+  # 추가할 내역만 추출
+  dfone = dfone[~dfone['Date'].isin(dfall['Date'])]
+
+  if not dfone.empty:
+    # 데이터프레임을 CSV 파일로 저장
+    my9_fname2 = '/my9_stock_out2_' + sCode + '.txt'
+    dfone.to_csv(my_dir + my9_fname2, index=False, header=False)
+
+    # 추가할 파일을 기존 파일에 병합
+    with open(my_dir + my9_fname2, 'r') as fone:
+      new_data = fone.read()
+      with open(my_dir + my9_fname, 'a') as fall:
+        fall.write(new_data)
+
+    # 임시 파일 삭제
+    os.remove(my_dir + my9_fname2)
+
+
+### 1. set mode
+# 1.1 mode 설정
+sMode = "Test"
+#sMode = "Real"
+
+my_dir, my_file = os.path.split(__file__)
+my_os = os.name
+
+# 1.2 filename 설정
+my_sfile = '/my9_stock_input.txt'
+
+# 1.3.1 Test 모드 (2페이지만 가져오기)
+if sMode == "Test":
+  max_loop = 3  # 2페이지만 가져오기 (1,2)
+
+# 1.3.2 Real 모드 (전체 가져오기)
+elif sMode == "Real":
+  max_loop = 0  # 전체 가져오기
+  if my_os == "nt":
+    my_dir = 'D:\MY_BLOG_LOCAL_HOME\py'
+  else:  # NT에서는 종료
+    print("여기서는 Real Mode가 안됩니다.")
+    exit()
+
+# 1.3.3 모드 이상 : 그냥 종료
+else:
+  print("sMode 이상")
+  exit()
+
+
+### 2. 데이터 전체 가져오기
+df = pd.read_csv(my_dir + my_sfile, dtype={'sCode': str})
 
 for sCode in df['sCode']:
-  get_all(sCode, 'FILE')   
-  print("get_all : ", sCode) 
-"""
+  # 파일 존재 여부 확인
+  my_filename = my_dir + '/my9_stock_out_' + sCode + '.txt' 
+  if os.path.exists(my_filename):
+    get_one(sCode)
+    print(sCode + " : 파일이 존재합니다.")
+  else:
+    get_all(sCode, 'FILE')   
+    print(sCode + " : 파일을 새로 만들었습니다")
 
-#""" #2 : 전체 종목 최근 정보 가져오기
+
+""" #2 : 전체 종목 최근 정보 가져오기
 df = pd.read_csv(my_dir + '/my9_stock_input.txt', dtype={'sCode': str})
 
 my9_fname = '/my9_stock_out_KOSPI.txt'
@@ -205,4 +338,4 @@ for sCode in df['sCode']:
   dfall = pd.concat([dfall, dfs], ignore_index=True)
 
 dfall.to_csv(my_dir + '/my9_stock_out_all.txt', index=False)
-# """
+"""
