61420a2e63b502c57474a84400c43003e8a15c99
new-sk <sookyun9@daum.net>
2025-04-26 11:22:09 +0900

    BLOG 250426L2 충돌중

--- MY_PGMB/py/py93_stock/my9_stock_v3.py
+++ MY_PGMB/py/py93_stock/my9_stock_v3.py
@@ -3,7 +3,7 @@ import my9_stock_naver as c9s
 my9 = c9s.cMy9Stock()
 
 my9.get_stocks()
-my9.xyz(['2025.02.28'])
+my9.xyz(['2025.03.13'])
 
 # 목표가 확인
 my9.ssStdAmt()
---
+++ MY_PGMB/py/py94_new_stock/my94_stock_v3.py
@@ -0,0 +1,11 @@
+import my94_stock_naver as c94s
+
+my94 = c94s.cMy94Stock()
+
+# 실제 작업은 다 막아
+my94.get_stocks()
+my94.xyz(['2025.04.16'])
+
+# 목표가 확인
+# my94.ssStdAmt()
+my94.ssStdAmt2()
